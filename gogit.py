853d122664f6b4a1be3ae217ff5e4af0fcefaf35
Steven <stevnn.yee@gmail.com>
2025-12-05 09:04:49 -0500

    really

--- gogit.py
+++ gogit.py
@@ -13,3 +13,7 @@ import logging.config
 import logging.handlers 
 
 logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting Go Git")
+    logger.info("Go Git is a simple version of git that allows you to create a repository and commit changes to it.")
